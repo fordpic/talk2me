9f574c030e98f9961f784eb3da187d954a4d30f9
Ford Pickert <fordpickert@gmail.com>
2023-10-03 19:12:04 -0500

    will continue to debug later

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -10,7 +10,7 @@ export async function POST(req: Request, res: NextResponse) {
 
 	const completion = await openai.chat.completions.create({
 		model: 'gpt-3.5-turbo',
-		messages: body.messages,
+		messages: [{ role: 'user', content: body.messages }],
 	});
 
 	console.log(completion.choices[0].message);
